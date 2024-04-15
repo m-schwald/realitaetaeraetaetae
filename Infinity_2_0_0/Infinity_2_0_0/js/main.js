f8ef8c5ed94cdc78f559b0b9ed6f9e5a81fb445b
Maizee <maize@freakmail.de>
2024-04-15 15:52:56 +0200

    fix js

--- Infinity_2_0_0/Infinity_2_0_0/js/main.js
+++ Infinity_2_0_0/Infinity_2_0_0/js/main.js
@@ -1,20 +1,7 @@
-/* ===================================================================
- * Infinity 2.0.0 - Main JS
- *
- *
- * ------------------------------------------------------------------- */
 
 (function(html) {
 
     'use strict';
-
-    const cfg = {
-        
-        // MailChimp URL
-        mailChimpURL : 'https://facebook.us1.list-manage.com/subscribe/post?u=1abf75f6981256963a47d197a&amp;id=37c6d8f4d6' 
-
-    };
-
 
     /* animations
     * -------------------------------------------------- */
@@ -141,27 +128,6 @@
     }; // end ssOffcanvas
 
 
-   /* masonry
-    * ------------------------------------------------------ */
-    const ssMasonry = function() {
-
-        const containerBricks = document.querySelector('.bricks');
-        if (!containerBricks) return;
-
-        imagesLoaded(containerBricks, function() {
-
-            const msnry = new Masonry(containerBricks, {
-                itemSelector: '.brick',
-                columnWidth: '.brick',
-                percentPosition: true,
-                resize: true
-            });
-
-        });
-
-    }; // end ssMasonry
-
-
    /* animate elements if in viewport
     * ------------------------------------------------------ */
     const ssAnimateOnScroll = function() {
@@ -204,274 +170,26 @@
     }; // end ssAnimateOnScroll
 
 
-   /* swiper
-    * ------------------------------------------------------ */ 
-    const ssSwiper = function() {
-
-        const clientsSwiper = new Swiper('.clients', {
-
-            slidesPerView: 3,
-            spaceBetween: 6,
-            slideClass: 'clients__slide',
-            pagination: {
-                el: '.swiper-pagination',
-                clickable: true,
-            },
-            breakpoints: {
-                // when window width is > 500px
-                501: {
-                    slidesPerView: 4,
-                    spaceBetween: 16
-                },
-                // when window width is > 900px
-                901: {
-                    slidesPerView: 5,
-                    spaceBetween: 32
-                },
-                // when window width is > 1200px
-                1201: {
-                    slidesPerView: 6,
-                    spaceBetween: 40
-                }
-            }
-        });
-
-        const testimonialsSwiper = new Swiper('.testimonial-slider', {
-
-            slidesPerView: 1,
-            effect: 'slide',
-            pagination: {
-                el: '.swiper-pagination',
-                clickable: true,
-            }
-        });
-
-    }; // end ssSwiper
-
-
     /* photoswipe
     * ----------------------------------------------------- */
     const ssPhotoswipe = function() {
-
-        const items = [];
-        const pswp = document.querySelectorAll('.pswp')[0];
         const folioItems = document.querySelectorAll('.folio-item');
 
-        if (!(pswp && folioItems)) return;
-
+        if (!folioItems) return;
+        
         folioItems.forEach(function(folioItem, i) {
-
+            
             let folio = folioItem;
             let thumb = folio.querySelector('.folio-item__thumb');
-            let thumbLink = folio.querySelector('.folio-item__thumb-link');
-            let title = folio.querySelector('.folio-item__title');
             let cat = folio.querySelector('.folio-item__cat');
-            let titleText = '<h4>' + title.innerHTML + '</h4>';
             folio.dataset.index = i;
-
             let catText = cat.innerHTML;
             const catElement = document.createElement('div');
             catElement.className = 'folio-item__thumb__cat';
             catElement.innerHTML = catText;
             thumb.appendChild(catElement);
-
-            let href = thumbLink.getAttribute('href');
-            let size = thumbLink.dataset.size.split('x'); 
-            let width  = size[0];
-            let height = size[1];
-
-            let item = {
-                src  : href,
-                w    : width,
-                h    : height
-            }
-
-            if (catElement) {
-                item.title = titleText.trim() + catText.trim();
-            }
-
-            items.push(item);
-
         });
-
-        // bind click event
-        folioItems.forEach(function(folioItem, i) {
-
-            let thumbLink = folioItem.querySelector('.folio-item__thumb-link');
-
-            thumbLink.addEventListener('click', function(event) {
-
-                event.preventDefault();
-
-                let options = {
-                    index: i,
-                    showHideOpacity: true
-                }
-            });
-
-        });
-
     };  // end ssPhotoSwipe
-
-
-   /* mailchimp form
-    * ---------------------------------------------------- */ 
-    const ssMailChimpForm = function() {
-
-        const mcForm = document.querySelector('#mc-form');
-
-        if (!mcForm) return;
-
-        // Add novalidate attribute
-        mcForm.setAttribute('novalidate', true);
-
-        // Field validation
-        function hasError(field) {
-
-            // Don't validate submits, buttons, file and reset inputs, and disabled fields
-            if (field.disabled || field.type === 'file' || field.type === 'reset' || field.type === 'submit' || field.type === 'button') return;
-
-            // Get validity
-            let validity = field.validity;
-
-            // If valid, return null
-            if (validity.valid) return;
-
-            // If field is required and empty
-            if (validity.valueMissing) return 'Please enter an email address.';
-
-            // If not the right type
-            if (validity.typeMismatch) {
-                if (field.type === 'email') return 'Please enter a valid email address.';
-            }
-
-            // If pattern doesn't match
-            if (validity.patternMismatch) {
-
-                // If pattern info is included, return custom error
-                if (field.hasAttribute('title')) return field.getAttribute('title');
-
-                // Otherwise, generic error
-                return 'Please match the requested format.';
-            }
-
-            // If all else fails, return a generic catchall error
-            return 'The value you entered for this field is invalid.';
-
-        };
-
-        // Show error message
-        function showError(field, error) {
-
-            // Get field id or name
-            let id = field.id || field.name;
-            if (!id) return;
-
-            let errorMessage = field.form.querySelector('.mc-status');
-
-            // Update error message
-            errorMessage.classList.remove('success-message');
-            errorMessage.classList.add('error-message');
-            errorMessage.innerHTML = error;
-
-        };
-
-        // Display form status (callback function for JSONP)
-        window.displayMailChimpStatus = function (data) {
-
-            // Make sure the data is in the right format and that there's a status container
-            if (!data.result || !data.msg || !mcStatus ) return;
-
-            // Update our status message
-            mcStatus.innerHTML = data.msg;
-
-            // If error, add error class
-            if (data.result === 'error') {
-                mcStatus.classList.remove('success-message');
-                mcStatus.classList.add('error-message');
-                return;
-            }
-
-            // Otherwise, add success class
-            mcStatus.classList.remove('error-message');
-            mcStatus.classList.add('success-message');
-        };
-
-        // Submit the form 
-        function submitMailChimpForm(form) {
-
-            let url = cfg.mailChimpURL;
-            let emailField = form.querySelector('#mce-EMAIL');
-            let serialize = '&' + encodeURIComponent(emailField.name) + '=' + encodeURIComponent(emailField.value);
-
-            if (url == '') return;
-
-            url = url.replace('/post?u=', '/post-json?u=');
-            url += serialize + '&c=displayMailChimpStatus';
-
-            // Create script with url and callback (if specified)
-            var ref = window.document.getElementsByTagName( 'script' )[ 0 ];
-            var script = window.document.createElement( 'script' );
-            script.src = url;
-
-            // Create global variable for the status container
-            window.mcStatus = form.querySelector('.mc-status');
-            window.mcStatus.classList.remove('error-message', 'success-message')
-            window.mcStatus.innerText = 'Submitting...';
-
-            // Insert script tag into the DOM
-            ref.parentNode.insertBefore( script, ref );
-
-            // After the script is loaded (and executed), remove it
-            script.onload = function () {
-                this.remove();
-            };
-
-        };
-
-        // Check email field on submit
-        mcForm.addEventListener('submit', function (event) {
-
-            event.preventDefault();
-
-            let emailField = event.target.querySelector('#mce-EMAIL');
-            let error = hasError(emailField);
-
-            if (error) {
-                showError(emailField, error);
-                emailField.focus();
-                return;
-            }
-
-            submitMailChimpForm(this);
-
-        }, false);
-
-    }; // end ssMailChimpForm
-
-
-   /* alert boxes
-    * ------------------------------------------------------ */
-    const ssAlertBoxes = function() {
-
-        const boxes = document.querySelectorAll('.alert-box');
-  
-        boxes.forEach(function(box){
-
-            box.addEventListener('click', function(event) {
-                if (event.target.matches('.alert-box__close')) {
-                    event.stopPropagation();
-                    event.target.parentElement.classList.add('hideit');
-
-                    setTimeout(function(){
-                        box.style.display = 'none';
-                    }, 500)
-                }
-            });
-        })
-
-    }; // end ssAlertBoxes
-
 
     /* Back to Top
     * ------------------------------------------------------ */
@@ -556,14 +274,9 @@
         ssMenuOnScrolldown();
         ssAnimateOnScroll();
         ssOffCanvas();
-        ssMasonry();
-        ssSwiper();
         ssPhotoswipe();
-        ssMailChimpForm();
-        ssAlertBoxes();
         ssBackToTop();
         ssMoveTo();
-
     })();
 
 })(document.documentElement);
